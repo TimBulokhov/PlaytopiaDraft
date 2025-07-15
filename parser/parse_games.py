8671db7f29f9635ea12ecf117d9b9d9f62524b91
TimBulokhov <tim.bulokhov@gmail.com>
2025-07-15 20:54:51 +0200

    Парсер: поддержка пагинации (до 50 стр), обновление games.json после каждой страницы; фронт: секция предзаказы, улучшения UI

--- parser/parse_games.py
+++ parser/parse_games.py
@@ -85,100 +85,119 @@ async def get_game_details(session, game_url):
 async def parse_playstation_async(url, region):
     import requests
     games = []
-    resp = requests.get(url, headers=HEADERS)
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    cards = soup.find_all('li', class_=re.compile('^psw-l-w-'))
-    game_links = []
-    for li in cards:
-        a = li.find('a', class_='psw-link')
-        if not a:
-            continue
-        # Название только из <span class='psw-t-body'>
-        title_tag = a.find('span', class_='psw-t-body')
-        title = title_tag.text.strip() if title_tag else 'No title'
-        # Картинка
-        img_url = ''
-        # Ищем img с data-qa, содержащим 'game-art#image#image'
-        img_tag = a.find('img', {'data-qa': re.compile('game-art#image#image')})
-        if img_tag:
-            srcset = img_tag.get('srcset', '')
-            if srcset:
-                srcset_items = [s.strip().split(' ')[0] for s in srcset.split(',')]
-                found_440 = [u for u in srcset_items if 'w=440' in u and 'thumb=false' in u]
-                found_230 = [u for u in srcset_items if 'w=230' in u and 'thumb=false' in u]
-                if found_440:
-                    img_url = found_440[0]
-                elif found_230:
-                    img_url = found_230[0]
+    page = 1
+    while page <= 50:
+        page_url = re.sub(r'/browse/\d+', f'/browse/{page}', url)
+        resp = requests.get(page_url, headers=HEADERS)
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        cards = soup.find_all('li', class_=re.compile('^psw-l-w-'))
+        if not cards:
+            break
+        game_links = []
+        for li in cards:
+            a = li.find('a', class_='psw-link')
+            if not a:
+                continue
+            # Название только из <span class='psw-t-body'>
+            title_tag = a.find('span', class_='psw-t-body')
+            title = title_tag.text.strip() if title_tag else 'No title'
+            # Картинка
+            img_url = ''
+            # Ищем img с data-qa, содержащим 'game-art#image#image'
+            img_tag = a.find('img', {'data-qa': re.compile('game-art#image#image')})
+            if img_tag:
+                srcset = img_tag.get('srcset', '')
+                if srcset:
+                    srcset_items = [s.strip().split(' ')[0] for s in srcset.split(',')]
+                    found_440 = [u for u in srcset_items if 'w=440' in u and 'thumb=false' in u]
+                    found_230 = [u for u in srcset_items if 'w=230' in u and 'thumb=false' in u]
+                    if found_440:
+                        img_url = found_440[0]
+                    elif found_230:
+                        img_url = found_230[0]
+                    else:
+                        img_url = img_tag.get('src', '')
                 else:
                     img_url = img_tag.get('src', '')
+            # Цена
+            price = ''
+            old_price = ''
+            discount = ''
+            # Парсим discount_percent из discount-badge
+            discount_badge = li.find('div', {'data-qa': re.compile('discount-badge')})
+            if discount_badge:
+                span = discount_badge.find('span')
+                if span and span.text.strip().startswith('-'):
+                    discount = span.text.strip()
+            # Цена
+            price_span = a.find('span', {'data-qa': re.compile('price#display-price')})
+            if price_span:
+                price = price_span.text.strip()
+            # old_price только из <span data-qa='price#original-price'> или <s>
+            old_price_tag = a.find('span', {'data-qa': re.compile('price#original-price')})
+            if not old_price_tag:
+                old_price_tag = a.find('s')
+            old_price = old_price_tag.text.strip() if old_price_tag else ''
+            subscription = ''
+            subscription_icon = ''
+            upsell_save = a.find('span', {'data-qa': re.compile('service-upsell#descriptorText')})
+            upsell_block = a.find_parent('div', class_='psw-service-upsell') or a.find('div', class_='psw-service-upsell')
+            if upsell_block:
+                if upsell_block.find('span', class_='psw-icon--3rd-party-ea'):
+                    subscription_icon = 'eaplay'
+                elif upsell_block.find('span', class_='psw-icon--ps-plus'):
+                    subscription_icon = 'psplus'
+            if upsell_save:
+                subscription = upsell_save.get_text(strip=True)
+            link = a.get('href')
+            if not link:
+                continue
+            full_link = 'https://store.playstation.com' + link
+            price_clean = price.strip().lower().replace(' ', '')
+            if not price_clean or price_clean in ['free', 'бесплатно']:
+                continue
+            game_links.append((full_link, title, img_url, price, old_price, discount, subscription, subscription_icon, region))
+        async with aiohttp.ClientSession() as session:
+            tasks = [get_game_details(session, link) for link, *_ in game_links]
+            details_list = await asyncio.gather(*tasks)
+        for (full_link, title, img_url, price, old_price, discount, subscription, subscription_icon, region), (release_date, voice_ps4, subtitles_ps4, voice_ps5, subtitles_ps5, platforms) in zip(game_links, details_list):
+            game = {
+                'title': title,
+                'img': img_url,
+                'price': price,
+                'old_price': old_price,
+                'discount_percent': discount,
+                'subscription': subscription,
+                'subscription_icon': subscription_icon,
+                'product_type': '',
+                'link': full_link,
+                'platforms': platforms,
+                'region': region,
+                'release_date': release_date
+            }
+            if 'PS4' in platforms:
+                game['voice_ps4'] = voice_ps4
+                game['subtitles_ps4'] = subtitles_ps4
+            if 'PS5' in platforms:
+                game['voice_ps5'] = voice_ps5
+                game['subtitles_ps5'] = subtitles_ps5
+            games.append(game)
+        # После каждой страницы сразу обновляем файл
+        try:
+            if os.path.exists(GAMES_JSON):
+                with open(GAMES_JSON, 'r', encoding='utf-8') as f:
+                    current = json.load(f)
             else:
-                img_url = img_tag.get('src', '')
-        # Цена
-        price = ''
-        old_price = ''
-        discount = ''
-        # Парсим discount_percent из discount-badge
-        discount_badge = li.find('div', {'data-qa': re.compile('discount-badge')})
-        if discount_badge:
-            span = discount_badge.find('span')
-            if span and span.text.strip().startswith('-'):
-                discount = span.text.strip()
-        # Цена
-        price_span = a.find('span', {'data-qa': re.compile('price#display-price')})
-        if price_span:
-            price = price_span.text.strip()
-        # old_price только из <span data-qa='price#original-price'> или <s>
-        old_price_tag = a.find('span', {'data-qa': re.compile('price#original-price')})
-        if not old_price_tag:
-            old_price_tag = a.find('s')
-        old_price = old_price_tag.text.strip() if old_price_tag else ''
-        subscription = ''
-        subscription_icon = ''
-        upsell_save = a.find('span', {'data-qa': re.compile('service-upsell#descriptorText')})
-        upsell_block = a.find_parent('div', class_='psw-service-upsell') or a.find('div', class_='psw-service-upsell')
-        if upsell_block:
-            if upsell_block.find('span', class_='psw-icon--3rd-party-ea'):
-                subscription_icon = 'eaplay'
-            elif upsell_block.find('span', class_='psw-icon--ps-plus'):
-                subscription_icon = 'psplus'
-        if upsell_save:
-            subscription = upsell_save.get_text(strip=True)
-        link = a.get('href')
-        if not link:
-            continue
-        full_link = 'https://store.playstation.com' + link
-        price_clean = price.strip().lower().replace(' ', '')
-        if not price_clean or price_clean in ['free', 'бесплатно']:
-            continue
-        game_links.append((full_link, title, img_url, price, old_price, discount, subscription, subscription_icon, region))
-    async with aiohttp.ClientSession() as session:
-        tasks = [get_game_details(session, link) for link, *_ in game_links]
-        details_list = await asyncio.gather(*tasks)
-    for (full_link, title, img_url, price, old_price, discount, subscription, subscription_icon, region), (release_date, voice_ps4, subtitles_ps4, voice_ps5, subtitles_ps5, platforms) in zip(game_links, details_list):
-        game = {
-            'title': title,
-            'img': img_url,
-            'price': price,
-            'old_price': old_price,
-            'discount_percent': discount,
-            'subscription': subscription,
-            'subscription_icon': subscription_icon,
-            'product_type': '',
-            'link': full_link,
-            'platforms': platforms,
-            'region': region,
-            'release_date': release_date
-        }
-        if 'PS4' in platforms:
-            game['voice_ps4'] = voice_ps4
-            game['subtitles_ps4'] = subtitles_ps4
-        if 'PS5' in platforms:
-            game['voice_ps5'] = voice_ps5
-            game['subtitles_ps5'] = subtitles_ps5
-        # Если только одна платформа, не добавляем вторую
-        games.append(game)
-    return games
+                current = []
+            current += games
+            with open(GAMES_JSON, 'w', encoding='utf-8') as f:
+                json.dump(current, f, ensure_ascii=False, indent=2)
+            games = []  # очищаем, чтобы не дублировать
+        except Exception as e:
+            print(f'Ошибка при записи в games.json: {e}')
+        print(f'Parsed page {page} ({len(game_links)} игр) для региона {region}')
+        page += 1
+    return []
 
 async def main_async():
     all_games = []
